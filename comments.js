7f99ac806efd764ebdf0134b2a8d1734a129fa35
VirulentVenom <107311505+VirulentVenom@users.noreply.github.com>
2025-02-05 16:52:20 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,29 @@
+// create web server
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const comments = require('./comments');
+
+http.createServer((req, res) => {
+  const { pathname, query } = url.parse(req.url, true);
+
+  if (pathname === '/api/comments' && req.method === 'GET') {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(comments));
+  } else if (pathname === '/api/comments' && req.method === 'POST') {
+    let data = '';
+    req.on('data', chunk => {
+      data += chunk;
+    });
+    req.on('end', () => {
+      const comment = JSON.parse(data);
+      comments.push(comment);
+      res.end(JSON.stringify(comment));
+    });
+  } else {
+    fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res);
+  }
+}).listen(3000, () => {
+  console.log('Server is running at http://localhost:3000/');
+});
